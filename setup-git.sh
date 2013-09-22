5d00fd46844bf3479fb53acd931d263dc6ce6e34
oxnz <yunxinyi@gmail.com>
2013-09-22 22:56:38 +0800

    Remove unused and add setup-git.sh

---
+++ setup-git.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git config --global user.name "John Doe"
+git config --global user.email johndoe@example.com
+git config --global core.editor emacs
+git config --global color.ui true
+git config --global core.pager less
+git config --global user.signingkey <gpg-key-id>
+git config --global push.default simple
