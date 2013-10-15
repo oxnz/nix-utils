6303e5e464fc32a0c2c1a7793ab9039fe6a92731
oxnz <yunxinyi@gmail.com>
2013-10-15 15:35:08 +0800

    add excludesfile to git-setup.sh

--- tools/git-setup.sh
+++ tools/git-setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# 全局提交用户名与邮箱
+# username & email
 git config --global user.name "Oxnz"
 git config --global user.email yunxinyi@gmail.com
 
 # 全局编辑器，提交时将COMMIT_EDITMSG编码转换成UTF-8可避免乱码
 git config --global core.editor emacs
+git config --global core.excludesfile ~/.gitignore
 git config --global color.ui true
 git config --global core.pager less
 git config --global user.signingkey <gpg-key-id>
@@ -18,4 +19,3 @@ echo "export LESSCHARSET=utf-8" > $HOME/.profile
 git config --global gui.encoding utf-8
 git config --global i18n.commitencoding utf-8
 git config --global i18n.logoutputencoding gbk
-
