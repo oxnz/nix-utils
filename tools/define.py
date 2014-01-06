5d5c86fa18847ce938d2498f1080de7a72581ac3
oxnz <yunxinyi@gmail.com>
2014-01-06 19:08:03 +0800

    replace old define.py

--- tools/define.py
+++ tools/define.py
@@ -1,111 +1,12 @@
 #! /usr/bin/python
+#-*- coding: utf-8 -*-
+
 import re;
 import urllib;
 import urllib2;
 import sys;
+from xml.dom import minidom
 
-def debug():
-    xml = open("word.xml").read();
-    print get_text(xml);
-    print get_elements_by_path(xml, "custom-translation/content");
-    #print_translations(xml, False, False);
-        
-def get_elements_by_path(xml, elem):
-    if type(xml) == type(''):
-        xml = [xml]
-        if type(elem) == type(''):
-            elem = elem.split('/')
-    if (len(xml) == 0):
-        return []
-    elif (len(elem) == 0):
-        return xml
-    elif (len(elem) == 1):
-        result = []
-        for item in xml:
-            result += get_elements(item, elem[0])
-        return result
-    else:
-        subitems = []
-        for item in xml:
-            subitems += get_elements(item, elem[0])
-        return get_elements_by_path(subitems, elem[1:])
-
-textre = re.compile("\!\[CDATA\[(.*?)\]\]", re.DOTALL)
-
-def get_text(xml):
-	match = re.search(textre, xml)
-	if not match:
-		return xml
-	return match.group(1)
-
-def get_elements(xml, elem):
-	p = re.compile("<" + elem + ">" + "(.*?)</" + elem + ">", re.DOTALL)
-	it = p.finditer(xml)
-	result = []
-	for m in it:
-		result.append(m.group(1));
-	return result;
-
-GREEN = "\033[1;32m";
-BLUE = "\033[1;34m";
-DEFAULT = "\033[0;49m";
-BOLD = "\033[1m";
-UNDERLINE = "\033[4m";
-NORMAL = "\033[m";
-RED = "\033[1;31m"
-
-def crawl_xml(queryword):
-	return urllib2.urlopen("http://dict.yodao.com/search?keyfrom=dict.python&q="
-        + urllib.quote_plus(queryword) + "&xmlDetail=true&doctype=xml").read();
-
-def print_translations(xml, with_color, detailed):
-        #print xml;
-	original_query = get_elements(xml, "original-query");
-	queryword = get_text(original_query[0]);
-	custom_translations = get_elements(xml, "custom-translation");
-	print BOLD + UNDERLINE + queryword + NORMAL;
-	translated = False;
-	
-	for cus in custom_translations:
-		source = get_elements_by_path(cus, "source/name");
-		
-		print NORMAL + "Translations from " + GREEN + source[0] + DEFAULT;
-		contents = get_elements_by_path(cus, "translation/content");
-		if with_color:
-			for content in contents[0:5]:
-				print BLUE + get_text(content) + DEFAULT;
-		else:
-			for content in contents[0:5]:
-				print get_text(content);
-		translated = True;
-
-	yodao_translations = get_elements(xml, "yodao-web-dict");
-	printed = False;
-	for trans in yodao_translations:
-                webtrans = get_elements(trans, "web-translation");
-		for web in webtrans[0:5]:
-			if not printed:
-				print NORMAL + "Translations from " + GREEN + "yodao:" + DEFAULT;
-				printed = True;
-	        	keys = get_elements(web, "key");
-			values = get_elements_by_path(web, "trans/value");
-			summaries = get_elements_by_path(web, "trans/summary");
-			key = keys[0].strip();
-			value = values[0].strip();
-			#summary = summaries[0].strip();
-                        #lines = get_elements(summary, "line");
-		        if with_color:
-			       	print BOLD +  get_text(key) + ":\t" +DEFAULT + BLUE + get_text(value) + NORMAL;
-                                #for line in lines:
-                                #        print GREEN + get_text(line) + DEFAULT;
-				#print get_text(summary) + DEFAULT;
-		        else:
-				print get_text(value);
-				#print get_text(summary);
-		        #translated = True;
-		        #if not detailed:
-			#        break
-	
 def usage(exit = True, ecode = 0):
     print """usage: dict options [word|"sentence"]
     Options:
@@ -121,12 +22,77 @@ def usage(exit = True, ecode = 0):
     if exit:
         sys.exit(ecode)
 
+def nodedump(node, skipNodeNames, transNodeNames):
+    class Enum(dict):
+        def __getattr__(self, name):
+            if name in self:
+                return self[name]
+            raise AttributeError
+    Color = Enum({
+        'red':  '\033[1;31m',
+        'green':'\033[1;32m',
+        'blue': '\033[1;34m',
+    })
+    Format = Enum({
+        'normal':   '\033[m',
+        'underline':'\033[4m',
+        'bold':     '\033[1m',
+        'default':  '\033[0;49m',
+    })
+    if node.nodeName in skipNodeNames:
+        pass
+    elif node.hasChildNodes():
+        #print node.nodeName
+        if node.nodeName in transNodeNames:
+            print Color.blue + Format.bold + transNodeNames[node.nodeName] \
+                    + ':' + Format.normal
+            del transNodeNames[node.nodeName]
+        for n in node.childNodes:
+            nodedump(n, skipNodeNames, transNodeNames)
+    elif node.nodeValue:
+        if node.nodeValue.startswith('http') and '://' in node.nodeValue:
+            print Format.underline + urllib.unquote(node.nodeValue.encode('utf-8')).decode('utf-8') + Format.normal
+        else:
+            print node.nodeValue.replace('<b>', Color.red+Format.bold) \
+                .replace('</b>', Format.normal)
+
+def translate(words, verbose=False):
+    xml = urllib2.urlopen("http://dict.yodao.com/search?keyfrom=dict.python&q="
+        + urllib.quote_plus(words) + "&xmlDetail=true&doctype=xml").read();
+    dom = minidom.parseString(xml)
+    skipNames = ['#text', 'yodao-link', 'sentence-speech']
+    if not verbose:
+        skipNames += ['example-sentences', 'yodao-web-dict', 'phonetic-symbol',
+                'return-phrase', 'sentence', 'sentence-translatoin',
+                'similar-words']
+    transNames = {
+            'key': '关键词',
+            #'trans': '翻译',
+            'summary': '概括',
+            'url': '链接',
+            'examples': '例子',
+            'similar-words': '相似单词',
+            #'translation': '翻译',
+            'word-form': '形式',
+            'phonetic-symbol': '音标',
+            'yodao-web-dict': '有道web词典',
+            'example-sentences': '例句',
+            'sentence-pair': '语句对',
+            'sentence': '句子',
+            'sentence-speech': '句子发音',
+            'sentence-translation': '句子翻译',
+    }
+    nodedump(dom.childNodes[0], skipNames, transNames)
+
 def main(argv):
-    if len(argv) <= 0:
+    verbose = False
+    if len(argv) <= 0 or (argv[0] == '-v' and len(argv) == 1):
         usage(True, 1)
+    if argv[0] == '-v':
+        verbose = True
+        del argv[0]
     try:
-        xml = crawl_xml(" ".join(argv))
-        print_translations(xml, with_color = True, detailed = True)
+        translate(" ".join(argv), verbose)
     except Exception as e:
         print e
 
