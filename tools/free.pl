47064bbeb1ffe4fd798c0f8fcef6a1402ce5f7eb
oxnz <yunxinyi@gmail.com>
2014-02-01 21:02:26 +0800

    add free as an alternative for linux free command

---
+++ tools/free.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my @vmstat = `vm_stat`;
+my %meminfo = ( 'Page size' => `sysctl -n hw.pagesize` );
+
+shift @vmstat;
+
+foreach (@vmstat) {
+	chomp;
+	my ($k, $v) = split /\s{2,}/;
+	$meminfo{$k} = int($v);
+}
+
+foreach (sort keys %meminfo) {
+	print $_,
+	' ' x (40 - length),
+	hsz($meminfo{$_} * $meminfo{'Page size'}), "\n";
+}
+
+# human-readable size
+sub hsz{
+	my $size = shift;
+	my @suffix = qw/B KB MB GB TB PB/;
+	return $size if not $size;
+	my $i = 0;
+	while ($size >= 1024) {
+		$size /= 1024;
+		++$i;
+	}
+	return sprintf("%-8.2f %s", $size, $suffix[$i]);
+}
