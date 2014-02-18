dad342bb8e3af897a5a56da47f98e98443b5910e
oxnz <yunxinyi@gmail.com>
2014-02-18 18:36:53 +0800

    refine time format

--- tools/fstat.pl
+++ tools/fstat.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use POSIX qw/strftime/;
 
 sub filestat {
 	my %fstat;
@@ -12,7 +13,7 @@ sub filestat {
 	$fstat{'uid'} .= "(" . scalar(getpwuid($fstat{'uid'})) . ")";
 	$fstat{'gid'} .= "(" . scalar(getgrgid($fstat{'gid'})) . ")";
 	foreach ("file", @statnames) {
-		$fstat{$_} = localtime $fstat{$_} if /^[amc]time$/;
+		$fstat{$_} = strftime "%T %F %Z", localtime $fstat{$_} if /^[amc]time$/;
 		print $_, " " x (10 - length), $fstat{$_}, "\n";
 	}
 }
