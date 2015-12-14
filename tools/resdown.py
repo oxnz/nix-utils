20c76d7839387267b6941f3c2952d4d7c2c4f270
云心逸 <yunxinyi+github@gmail.com>
2015-12-14 20:15:27 +0800

    update dpics -> resdown

--- tools/resdown.py
+++ tools/resdown.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+''' A simple resource download tool'''
+
 import urllib
 import urllib2
 import re
@@ -8,14 +10,17 @@ import sys
 import os
 import threading
 
-pattern = re.compile('(http.*?(\d{6})/(\d+)/(\d+)\.jpg)')
-
 class Downloader(threading.Thread):
     def __init__(self, url, path, fname):
         super(Downloader, self).__init__()
         self.url = url
         self.path = path
+        self._fname = fname
         self.fpath = os.path.join(path, fname)
+
+    @property
+    def fname(self):
+        return self._fname
 
     def run(self):
         try:
@@ -28,26 +33,30 @@ class Downloader(threading.Thread):
                 raise e
         urllib.urlretrieve(self.url, self.fpath)
 
-def downpics(url):
+def downpics(url, pattern):
     html = urllib2.urlopen(url).read()
+    fname = url[url.rindex('/')+1:]
+    with open(fname, 'w') as f:
+        f.write(html)
     taskq = []
     for (url, pdname, dname, fname) in pattern.findall(html):
-        taskq.append(Downloader(url,
-            os.path.join(pdname, dname), fname + '.jpg'))
+        taskq.append(Downloader(url, os.path.join(pdname, dname), fname))
+        sys.stdout.write('+ {}\n'.format(fname))
     for task in taskq:
         task.start()
     for task in taskq:
         task.join()
-        sys.stdout.write('-')
+        sys.stdout.write('- {}\n'.format(task.fname))
         sys.stdout.flush()
-    sys.stdout.write('\n')
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print 'Usage: \n\t', sys.argv[0], '<url>'
+    if len(sys.argv) < 3:
+        print 'Usage: {} <pattern> <url>'.format(sys.argv[0])
         sys.exit(1)
-    for url in sys.argv[1:]:
+    pattern = re.compile('(http.*?(\d{6})/(\d+)/(\d+\.jpg))')
+    #pattern = re.compile(sys.argv[1])
+    for url in sys.argv[2:]:
         try:
-            downpics(url)
+            downpics(url, pattern)
         except Exception, e:
             print e
